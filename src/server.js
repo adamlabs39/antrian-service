e96a9558a86913facf72fb1e4097914fe6426b16
ganadipa <nyomanganadipa@gmail.com>
2025-01-30 15:46:54 +0700

    feat (structure); initial code structure

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import "./configurations/env.js";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
