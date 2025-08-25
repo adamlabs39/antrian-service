3070f622393cc1351408f0e78a617f8f1993dea7
Engineer Adamlabs <husni@adamlabs.id>
2025-08-25 03:49:07 +0000

    fix

--- src/server.js
+++ src/server.js
@@ -83,7 +83,6 @@ app.use(errorHandler);
 // Start the server.
 app.listen(APPLICATION_PORT, APPLICATION_HOST, async () => {
   // Log telling that the server is successfully running.
-  console.log(process.env)
   console.log(
     `Server is running on http://${APPLICATION_HOST}:${APPLICATION_PORT}`
   );
