0b671388f02df3108a9b6c1762504b75d32df16f
nafisaa11 <jnafisah50@gmail.com>
2025-09-10 17:07:28 +0700

    fix: memperbaiki handling jika pasien memasukkan string kosong pada apm farmasi

--- src/routes/report-antrian.route.js
+++ src/routes/report-antrian.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { ReportAntrianController } from "../controllers/report-antrian.controller.js";
+// import { ReportAntrianController } from "../controllers/report-antrian.controller.js";
 
 export const ReportAntrianRouter = express.Router();
 
-ReportAntrianRouter.post("/cancle-booking", ReportAntrianController.cancelBooking);
+// ReportAntrianRouter.post("/cancle-booking", ReportAntrianController.cancelBooking);
