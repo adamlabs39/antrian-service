a64d9af6804b729797e3051a83e4ad476060d03f
nafisaa11 <jnafisah50@gmail.com>
2025-09-11 16:09:00 +0700

    fix: memperbaiki format data menggunaka snake case

--- src/controllers/report-antrian.controller.js
+++ src/controllers/report-antrian.controller.js
@@ -1,32 +1,15 @@
-import { BadRequestException } from "../exceptions/bad-request.exception.js";
 import { ReportAntrianService } from "../services/report-antrian.service.js";
 
 export class ReportAntrianController {
-  /**
-   * Mengurangi jumlah antrian aktif saat booking dibatalkan.
-   */
   static async cancelBooking(req, res, next) {
     try {
-      const { jadwalDokterUuid, tanggalPelayanan } = req.body;
+      const result = await ReportAntrianService.cancelBooking(req.body);
 
-      // Validasi input
-      if (!jadwalDokterUuid || !tanggalPelayanan) {
-        throw new BadRequestException(
-          "jadwalDokterUuid dan tanggalPelayanan wajib diisi"
-        );
-      }
-
-      const result = await ReportAntrianService.cancelBooking({
-        jadwalDokterUuid,
-        tanggalPelayanan,
-      });
-    
-      // Pesan dan data respons disesuaikan dengan logika baru
       return res.json({
         success: true,
         message: "Jumlah antrian aktif berhasil diperbarui.",
         data: {
-          jumlahAntrianAktif: result.jumlahAntrianAktif,
+          jumlah_anntrian_aktif: result.jumlahAntrianAktif,
         },
       });
     } catch (err) {
